8a50758f37e40661b8f38769cdb606f42c9cc2c0
renawmontanari <regiangelica@gmail.com>
2022-08-15 14:02:47 -0300

    Convertendo String para Boolean em JavaScript

---
+++ converterStringParaBoolean.js
@@ -0,0 +1,21 @@
+
+'use strict'
+
+const x ='true';
+const y = 'false';
+
+console.log(x);
+console.log(typeof x);
+
+// Conversão de tipo.
+const xBollean = x === 'true';
+
+console.log(xBollean);
+console.log(typeof xBollean);
+
+const yBollean = y === 'true';
+
+console.log(xBollean);
+console.log(typeof xBollean);
+
+// Comparações geram booleanos, retornam booleanos.
